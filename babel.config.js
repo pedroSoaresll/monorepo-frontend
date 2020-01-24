91202026fce94f5a6e0ccf46c44112f9be0bdc01
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-01-24 19:47:29 -0300

    remove unused project + fix jest + create new test example

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: ["babel-plugin-styled-components", "inline-react-svg"],
   presets: [
     "@babel/preset-typescript",
-    "@babel/preset-react"
+    "@babel/preset-react",
+    "@babel/preset-env"
   ]
 };
